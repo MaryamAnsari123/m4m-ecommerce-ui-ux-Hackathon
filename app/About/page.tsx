356e2f59462c7b92e88f0d1229994bc6d3e2bcd5
Maryam Ansari <163015076+MaryamAnsari123@users.noreply.github.com>
2024-12-13 23:12:42 +0500

    Update page.tsx

--- app/About/page.tsx
+++ app/About/page.tsx
@@ -9,6 +9,22 @@ import About from '../Components/About'
 const page = () => {
   return (
     <div className='justify-items-center'>
+
+            <div className='lg:w-[1440px] p-2 w-[270px] h-[277px] flex flex-col lg:flex-row 
+      lg:gap-48 gap-4 justify-center items-center'>
+
+        <p className='lg:text-2xl text-lg'>
+        A brand built on the love of craftmanship, <br />
+        quality and outstanding customer service
+        </p>
+
+        <button className='bg-gray-400 hover:bg-blue-300 hover:shadow-lg 
+            py-2 px-2 font-medium text-base lg:w-[192px] w-[250px] h-[56px]'>
+                View Collection
+            </button>
+
+      </div >
+      
         <div>
       <Card1 />
       </div>
